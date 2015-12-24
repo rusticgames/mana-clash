909a9fcb542bf291d7f2d57d175c8bdc6d5a5a15
Nicholas Scheurich <nick@scheurich.me>
2015-12-23 21:50:09 -0600

    Initial commit

---
+++ Assets/ForceBall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceBall : MonoBehaviour
+{
+
+  public float moveSpeed;
+  public int moveDir;
+
+  private Rigidbody2D m_Rigidbody2D;
+
+  private void Awake()
+  {
+    m_Rigidbody2D = GetComponent<Rigidbody2D>();
+  }
+
+	void Update () 
+  {
+    m_Rigidbody2D.velocity = new Vector2(moveSpeed * moveDir, m_Rigidbody2D.velocity.y);
+	}
+
+  void OnCollisionEnter2D(Collision2D coll)
+  {
+    GameObject.Destroy(gameObject, 0.1f);
+  }
+}
---
+++ Assets/ProjectileLauncher.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+public class ProjectileLauncher : MonoBehaviour
+{
+
+  public GameObject projectile;
+  public int projectileDir = 1;
+
+  private PlatformerCharacter2D platformerCharacter2D;
+
+  void Awake()
+  {
+    platformerCharacter2D = GetComponent<PlatformerCharacter2D>();
+  }
+
+  void Update()
+  {
+    projectileDir = platformerCharacter2D.m_FacingRight ? 1 : -1;
+
+    if (CrossPlatformInputManager.GetButtonDown("[" + gameObject.name + "] Fire")) {
+      Vector3 projectilePos = gameObject.transform.position;
+      projectilePos.x = projectilePos.x + projectileDir;
+      GameObject forceBall = GameObject.Instantiate(projectile, projectilePos, Quaternion.identity) as GameObject;
+      forceBall.GetComponent<ForceBall>().moveDir = projectileDir;
+    }
+	}
+}
