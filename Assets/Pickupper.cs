86d0953a913d270f3c1333cb60174161fe6ac9cb
jkwascom <jkwascom@gmail.com>
2015-12-30 20:01:41 -0600

    pickup-y stuff
    shitty first draft of pickups

---
+++ Assets/Pickupper.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets._2D;
+
+public class Pickupper : MonoBehaviour {
+    public bool running = true;
+    public Collider2D grabCollider;
+    public Joint2D attachJoint;
+    public bool grabbing = false;
+    private PlatformerCharacter2D platformerCharacter2D;
+
+    public string inputId;
+
+    void Reset()
+    {
+        inputId = "[" + gameObject.name + "] Grab";
+    }
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(pickupCheck());
+    }
+
+    IEnumerator pickupCheck()
+    {
+        while (running)
+        {
+            grabbing = false;
+            if (CrossPlatformInputManager.GetButtonDown(inputId))
+            {
+                while (CrossPlatformInputManager.GetButton(inputId))
+                {
+                    grabbing = true;
+                    yield return null;
+                }
+                grabbing = false;
+                if(grabCollider != null)
+                {
+                    break;
+                }
+
+            }
+            yield return null;
+        }
+        yield return null;
+    }
+
+    IEnumerator dropCheck()
+    {
+        while (running)
+        {
+            if (CrossPlatformInputManager.GetButtonDown(inputId))
+            {
+                GameObject.Destroy(attachJoint);
+
+                var projectileDir = platformerCharacter2D.m_FacingRight ? 1 : -1;
+                Vector3 projectilePos = gameObject.transform.position;
+                projectilePos.x = projectilePos.x + projectileDir;
+                grabCollider.transform.position = projectilePos;
+                grabCollider.isTrigger = false;
+                grabCollider = null;
+
+                while (CrossPlatformInputManager.GetButton(inputId))
+                    yield return null;
+                StartCoroutine(pickupCheck());
+                break;
+            }
+            yield return null;
+        }
+        yield return null;
+    }
+
+    void Awake()
+    {
+        platformerCharacter2D = GetComponent<PlatformerCharacter2D>();
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (grabbing)
+        {
+            var pickup = coll.gameObject.GetComponent<Pickuppable>();
+            if (pickup != null)
+            {
+                grabCollider = coll.collider;
+                grabbing = false;
+                coll.collider.transform.position = gameObject.transform.position;
+                var joint = gameObject.AddComponent<DistanceJoint2D>();
+                joint.enableCollision = false;
+                coll.collider.isTrigger = true;
+                joint.connectedBody = coll.collider.GetComponent<Rigidbody2D>();
+                joint.distance = 0f;
+                attachJoint = joint;
+                StartCoroutine(dropCheck());
+            }
+            //coll.gameObject.SendMessage("ApplyDamage", 10);
+        }
+
+    }
+}
--- Assets/ProjectileLauncher.cs
+++ Assets/ProjectileLauncher.cs
@@ -6,25 +6,30 @@ using UnityStandardAssets._2D;
 public class ProjectileLauncher : MonoBehaviour
 {
 
-  public GameObject projectile;
-  public int projectileDir = 1;
+    public GameObject projectile;
+    public int projectileDir = 1;
+    private PlatformerCharacter2D platformerCharacter2D;
 
-  private PlatformerCharacter2D platformerCharacter2D;
+    public string inputId;
 
-  void Awake()
-  {
-    platformerCharacter2D = GetComponent<PlatformerCharacter2D>();
-  }
-
-  void Update()
-  {
-    projectileDir = platformerCharacter2D.m_FacingRight ? 1 : -1;
-
-    if (CrossPlatformInputManager.GetButtonDown("[" + gameObject.name + "] Fire")) {
-      Vector3 projectilePos = gameObject.transform.position;
-      projectilePos.x = projectilePos.x + projectileDir;
-      GameObject forceBall = GameObject.Instantiate(projectile, projectilePos, Quaternion.identity) as GameObject;
-      forceBall.GetComponent<ForceBall>().moveDir = projectileDir;
+    void Reset()
+    {
+        inputId = "[" + gameObject.name + "] Fire";
     }
-	}
+    void Awake()
+    {
+        platformerCharacter2D = GetComponent<PlatformerCharacter2D>();
+    }
+
+    void Update()
+    {
+        projectileDir = platformerCharacter2D.m_FacingRight ? 1 : -1;
+        if (CrossPlatformInputManager.GetButtonDown(inputId))
+        {
+            Vector3 projectilePos = gameObject.transform.position;
+            projectilePos.x = projectilePos.x + projectileDir;
+            GameObject forceBall = GameObject.Instantiate(projectile, projectilePos, Quaternion.identity) as GameObject;
+            forceBall.GetComponent<ForceBall>().moveDir = projectileDir;
+        }
+    }
 }
