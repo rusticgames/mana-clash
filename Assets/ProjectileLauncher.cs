4da2c53ba02cb5fd75ef43b8b3d7ceb1104cc4b8
Nicholas Scheurich <nick@scheurich.me>
2015-12-30 20:07:04 -0600

    Add charged shot

--- Assets/ProjectileLauncher.cs
+++ Assets/ProjectileLauncher.cs
@@ -7,7 +7,10 @@ public class ProjectileLauncher : MonoBehaviour
 {
 
   public GameObject projectile;
+  public bool isCharger;
   public int projectileDir = 1;
+  public float chargePower = 20.0f;
+  public float chargedTime = 0f;
 
   private PlatformerCharacter2D platformerCharacter2D;
 
@@ -20,11 +23,20 @@ public class ProjectileLauncher : MonoBehaviour
   {
     projectileDir = platformerCharacter2D.m_FacingRight ? 1 : -1;
 
-    if (CrossPlatformInputManager.GetButtonDown("[" + gameObject.name + "] Fire")) {
+    if (isCharger && CrossPlatformInputManager.GetButton("[" + gameObject.name + "] Fire")) {
+      chargedTime = chargedTime + Time.deltaTime;
+    }
+
+    if (CrossPlatformInputManager.GetButtonUp("[" + gameObject.name + "] Fire")) {
       Vector3 projectilePos = gameObject.transform.position;
       projectilePos.x = projectilePos.x + projectileDir;
       GameObject forceBall = GameObject.Instantiate(projectile, projectilePos, Quaternion.identity) as GameObject;
       forceBall.GetComponent<ForceBall>().moveDir = projectileDir;
+
+      if (isCharger) {
+        forceBall.GetComponent<ForceBall>().moveSpeed = chargedTime * chargePower;
+        chargedTime = 0f;
+      }
     }
 	}
 }
